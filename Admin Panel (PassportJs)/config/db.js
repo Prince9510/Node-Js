a9aa613e17d048d2d1ba77c1089644ae0aad11e0
Prince9510 <131976898+Prince9510@users.noreply.github.com>
2025-02-27 14:02:11 +0530

    Delete Admin Panel (PassportJs) directory

--- Admin Panel (PassportJs)/config/db.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require ("mongoose");
-mongoose.connect('mongodb+srv://satasiyaprince9510:8160636847@free.t64lj.mongodb.net/?retryWrites=true&w=majority&appName=ADMIN');
-const db = mongoose.connection;
-db.once("open",(err)=>{
-    err?console.log(err):console.log("Connected to MongoDB");    
-})
-
-module.exports = db;
