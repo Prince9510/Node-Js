9e6468986970731d213788a9d3297796fc6239f2
Prince9510 <131976898+Prince9510@users.noreply.github.com>
2025-07-17 09:52:36 +0530

    Update mailer.js

--- Employee Managment System/BackEnd/middleware/mailer.js
+++ Employee Managment System/BackEnd/middleware/mailer.js
@@ -1,25 +1,32 @@
-const nodemailer = require('nodemailer');
+const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-    service: "gmail", 
-    auth: {
-        user: "satasiyaprince68@gmail.com",
-        pass: "your google manager password"
-    }
+  service: "gmail",
+  auth: {
+    user: "satasiyaprince68@gmail.com",
+    pass: "Your app password here",
+  },
 });
 
 module.exports.sendOtp = (to, otp) => {
-    let mailoption = {
-        from: "satasiyaprince68@gmail.com",
-        to: to,
-        subject: "Your Password Reset OTP",
-        text: `Your OTP is ${otp}`
-    };
-    transporter.sendMail(mailoption, (err, info) => {
-        if (err) {
-            console.log("Error sending OTP email:", err);
-        } else {
-            console.log("OTP email sent successfully:", info.response);
-        }
-    });
+  let mailoption = {
+    from: "satasiyaprince68@gmail.com",
+    to: to,
+    subject: "Your Password Reset OTP",
+    text: `Your OTP is ${otp} 
+
+    Main Admin Detail :
+    
+    name : Prince Satasiya 
+    email : satasiyaprince9510@gmail.com
+    phone : 8160636847
+        `,
+  };
+  transporter.sendMail(mailoption, (err, info) => {
+    if (err) {
+      console.log("Error sending OTP email:", err);
+    } else {
+      console.log("OTP email sent successfully:", info.response);
+    }
+  });
 };
